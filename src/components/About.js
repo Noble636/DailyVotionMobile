f56a523f6582ed3652c4be390790b06f5b70eaf4
Noble636 <nikkoarangorin004@gmail.com>
2025-12-31 10:12:57 +0800

    Initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -23,6 +23,7 @@ function About() {
           align-items: center;
           width: 100vw;
           max-width: 100vw;
+          margin: 0 auto;
           padding: 0 1rem;
         }
         .aboutpage-text {
